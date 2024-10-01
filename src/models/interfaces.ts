3ee200e7945cafb34f53216146f3f0c0a773c47a
Matias Oliva <olivamati@gmail.com>
2024-10-01 21:49:14 +0000

    add a new data structure

---
+++ src/models/interfaces.ts
@@ -0,0 +1,14 @@
+export interface ImageGallery {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+}
+
+export interface IProject {
+  title: string;
+  description: string;
+  cover: string;
+  imageGallery: ImageGallery[];
+  headerImage: string;
+}
