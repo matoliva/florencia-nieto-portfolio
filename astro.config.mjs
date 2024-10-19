6cc7f2591f92f3f9ce8cf26f4719636d5fdd6ff2
Matias Oliva <olivamati@gmail.com>
2024-10-19 19:08:02 +1300

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
